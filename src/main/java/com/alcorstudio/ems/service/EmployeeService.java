f6e6476ede02526753bb2a91cb3ae955ec5750c8
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-12 03:29:39 +0300

    Added getting  all employees list

--- src/main/java/com/alcorstudio/ems/service/EmployeeService.java
+++ src/main/java/com/alcorstudio/ems/service/EmployeeService.java
@@ -2,10 +2,15 @@ package com.alcorstudio.ems.service;
 
 import com.alcorstudio.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     //GetEmployee
     EmployeeDto getEmployeeById(Long employeeId);
+
+    //GetAllEmployees
+    List<EmployeeDto> getAllEmployees();
 }
