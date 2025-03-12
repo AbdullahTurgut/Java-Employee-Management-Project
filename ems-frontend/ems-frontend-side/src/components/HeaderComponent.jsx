e0f5f23db0442681d7c0e20c4e0fc59eb2f71873
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-13 02:44:56 +0300

    Header and Footer components added

---
+++ ems-frontend/ems-frontend-side/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav class="navbar navbar-dark bg-dark">
+                    <a className="navbar-brand" href="https://www.alcorguide.net">Employee Management System</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
